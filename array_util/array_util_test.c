43cb46bbdcf720f29875f571201eda271a81ff2e
BugDiver <vinaysh@thoughtworks.com>
2016-01-08 10:55:09 +0530

    remove main function from testfile so that test can run with runtest framework

--- array_util/array_util_test.c
+++ array_util/array_util_test.c
@@ -7,7 +7,7 @@
 
 
 
-void test_create_array_util_for_int(int *num){
+void test_create_array_util_for_int(){
    ArrayUtil scores = create(4 ,5);
    assert(scores.typeSize == 4);
    assert(scores.length == 5);
@@ -15,10 +15,9 @@ void test_create_array_util_for_int(int *num){
    a[0] = 95;
    assert(a[0] == 95);
    dispose(scores);
-   (*num)++;
 }
 
-void test_create_array_util_for_float(int *num){
+void test_create_array_util_for_float(){
    ArrayUtil weight = create(8,5);
    assert(weight.typeSize == 8);
    assert(weight.length == 5);
@@ -26,11 +25,10 @@ void test_create_array_util_for_float(int *num){
    b[0] = 10.5;
    assert(b[0] == 10.5);
    dispose(weight);
-   (*num)++;
 }
 
 
-void test_create_array_util_for_char(int *num){
+void test_create_array_util_for_char(){
    ArrayUtil name = create(1,5);
    assert(name.typeSize == 1);
    assert(name.length == 5);
@@ -46,10 +44,9 @@ void test_create_array_util_for_char(int *num){
    assert(strcmp("hello" ,c) == 0);
 
    dispose(name);
-   (*num)++;
 }
 
-void test_are_equal_for_int_type_array_util(int *num){
+void test_are_equal_for_int_type_array_util(){
    ArrayUtil score1 = create(4 ,5);
    ArrayUtil score2 = create(4 ,5);
    int *s1 = (int *)score1.base;
@@ -59,13 +56,12 @@ void test_are_equal_for_int_type_array_util(int *num){
 
    int deff = areEqual(score1 ,score2);
    assert(deff == 0);
-   (*num)++;
 
    dispose(score1);
    dispose(score2);
 }
 
-void test_are_equal_for_char_type_array_util(int *num){
+void test_are_equal_for_char_type_array_util(){
    ArrayUtil f_name = create(1,5);
    char *f = (char *)f_name.base;
    f[0] = 'h';
@@ -84,33 +80,30 @@ void test_are_equal_for_char_type_array_util(int *num){
 
    int validation = areEqual(f_name , s_name);
    assert(validation == 1);
-   (*num)++;
 
    dispose(f_name);
    dispose(s_name);
 }
 
-void test_resize_array_util_for_int_type(int *num){
+void test_resize_array_util_for_int_type(){
    ArrayUtil scores = create(4,5);
    int *s  = (int *)scores.base;
    s[0] = 92; s[1] = 90; s[2] = 45; s[3] = 76; s[4] = 54;
    scores = resize(scores ,3);
    assert(scores.length == 3);
-   (*num)++;
    dispose(scores);
 }
 
-void test_resize_array_util_for_char_type(int *num){
+void test_resize_array_util_for_char_type(){
    ArrayUtil name = create(1,5);
    char *s  = (char *)name.base;
    s[0] = 'v'; s[1] = 'i'; s[2] = 'n'; s[3] = 'a'; s[4] = 'y';
    name = resize(name ,3);
    assert(name.length == 3);
-   (*num)++;
    dispose(name);
 }
 
-void test_find_index_for_int_type(int *num){
+void test_find_index_for_int_type(){
    ArrayUtil scores = create(4,5);
    int *s  = (int *)scores.base;
    s[0] = 92; s[1] = 90; s[2] = 45; s[3] = 76; s[4] = 54;
@@ -123,11 +116,10 @@ void test_find_index_for_int_type(int *num){
    assert(index_of_y == 0);
    assert(index_of_z == 3);
    assert(index_of_a == -1);
-   (*num)++;
    dispose(scores);
 }
 
-void test_find_index_for_char_type(int *num){
+void test_find_index_for_char_type(){
    ArrayUtil name = create(4,5);
    int *s  = (int *)name.base;
    s[0] = 'h'; s[1] = 'e'; s[2] = 'l'; s[3] = 'l'; s[4] = 'o';
@@ -144,7 +136,6 @@ void test_find_index_for_char_type(int *num){
    assert(index_of_z == 2);
    assert(index_of_a == -1);
 
-   (*num)++;
    dispose(name);
 }
 
@@ -156,7 +147,7 @@ int divisibleBy(void *num1 ,void *num2){
    return 0;
 }
 
-void test_find_first_for_int_type(int * num){
+void test_find_first_for_int_type(){
    ArrayUtil integers = create(sizeof(int),5);
    int *s  = (int *)integers.base;
    s[0] = 15, s[1] = 2, s[2] = 3, s[3] = 4, s[4] = 5;
@@ -166,7 +157,6 @@ void test_find_first_for_int_type(int * num){
    int divider2 = 5;
    int first_multiple_of_5 = *(int *)findFirst(integers ,divisibleBy , &divider2);
    assert(first_multiple_of_5 = 15);
-   (*num)++;
 };
 
 char to_lower(char character){
@@ -184,17 +174,16 @@ int is_vowel(void *hint ,void *my_char){
 }
 
 
-void test_find_first_for_char_type(int * num){
+void test_find_first_for_char_type(){
    ArrayUtil name = create(sizeof(char),5);
    char *s  = (char *)name.base;
    s[0] ='h', s[1] = 'e', s[2] = 'l', s[3] = 'l', s[4] = 'o';
    char a = 'a';
    char f_vowel = *(char *)findFirst(name ,is_vowel , &a);
    assert(f_vowel == 'e');
-   (*num)++;
 }
 
-void test_find_last_for_int_type(int * num){
+void test_find_last_for_int_type(){
    ArrayUtil integers = create(sizeof(int),5);
    int *s  = (int *)integers.base;
    s[0] = 15, s[1] = 2, s[2] = 3, s[3] = 4, s[4] = 5;
@@ -204,37 +193,33 @@ void test_find_last_for_int_type(int * num){
    int divider2 = 5;
    int last_multiple_of_5 = *(int *)findLast(integers ,divisibleBy , &divider2);
    assert(last_multiple_of_5 = 5);
-   (*num)++;
 };
 
-void test_find_last_for_char_type(int * num){
+void test_find_last_for_char_type(){
    ArrayUtil name = create(sizeof(char),5);
    char *n  = (char *)name.base;
    n[0] ='h', n[1] = 'e', n[2] = 'l', n[3] = 'l', n[4] = 'o';
    char a = 'a';
    char l_vowel = *(char *)findLast(name ,is_vowel , &a);
    assert(l_vowel == 'o');
-   (*num)++;
 }
-void test_count_for_int_type(int *num){
+void test_count_for_int_type(){
    ArrayUtil integers = create(sizeof(int),5);
    int *s  = (int *)integers.base;
    s[0] = 15, s[1] = 2, s[2] = 6, s[3] = 4, s[4] = 5;
    int divider1 = 2;
    int no_of_even_numbers = count(integers ,divisibleBy , &divider1);
    assert(no_of_even_numbers == 3);
-   (*num)++;
 
 };
 
-void test_count_for_char_type(int * num){
+void test_count_for_char_type(){
    ArrayUtil name = create(sizeof(char),5);
    char *n  = (char *)name.base;
    n[0] ='h', n[1] = 'e', n[2] = 'l', n[3] = 'l', n[4] = 'o';
    char a = 'a';
    int count_of_vowel = count(name ,is_vowel , &a);
    assert(count_of_vowel == 2);
-   (*num)++;
 }
 
 int is_even(void *hint, void *item){
@@ -252,7 +237,7 @@ int is_greater_than(void *hint ,void *num){
    return 0;
 }
 
-void test_filter_array_util_for_int_type(int *num){
+void test_filter_array_util_for_int_type(){
    ArrayUtil scores = create(sizeof(int) ,6);
    int *s = (int *)scores.base;
    s[0] = 92; s[1] = 45; s[2] = 90; s[3] = 76; s[4] = 54, s[5] = 73;
@@ -260,12 +245,11 @@ void test_filter_array_util_for_int_type(int *num){
    ArrayUtil destination = create(sizeof(int),4);
    int no_of_distinction = filter(scores ,&is_greater_than ,&hint, destination.base ,destination.length);
    assert(no_of_distinction == 3);
-   (*num)++;
 }
 
 
 
-void test_filter_should_add_elements_into_given_array(int * num){
+void test_filter_should_add_elements_into_given_array(){
    ArrayUtil a = create(4,5);
    int * list_array = (int *)(a.base);
    list_array[0] = 12;
@@ -279,10 +263,9 @@ void test_filter_should_add_elements_into_given_array(int * num){
    int **d = ((int **)dest.base);
    assert(*d[0] == 12);
    assert(*d[1] == 34);
-   (*num)++;
 
 };
-void test_filter_should_add_the_addresses_of_elements_into_given_array(int * num){
+void test_filter_should_add_the_addresses_of_elements_into_given_array(){
    ArrayUtil a = create(1,5);
    char * list_array = (char *)(a.base);
    list_array[0] = 'a';
@@ -297,7 +280,6 @@ void test_filter_should_add_the_addresses_of_elements_into_given_array(int * num
    char **d = (char **)dest.base;
    assert(*d[0] == 'a');
    assert(*d[1] == 'e');
-   (*num)++;
 };
 
 void increment_by(void *hint, void *sourceItem, void *destinationItem){
@@ -307,7 +289,7 @@ void increment_by(void *hint, void *sourceItem, void *destinationItem){
    *((int *)destinationItem) = *num;
 };
 
-void test_map_should_copy_converted_element_in_destination(int * num){
+void test_map_should_copy_converted_element_in_destination(){
    ArrayUtil source = create(sizeof(int),5);
    int * list = (int *)(source.base);
    list[0] = 12;
@@ -327,7 +309,6 @@ void test_map_should_copy_converted_element_in_destination(int * num){
    assert(d[2] == 5);
    assert(d[3] == 6);
    assert(d[4] == 7);
-   (*num)++;
 };
 
 void rot_13(void *hint, void *sourceItem, void *destinationItem){
@@ -339,7 +320,7 @@ void rot_13(void *hint, void *sourceItem, void *destinationItem){
    *((char *)destinationItem) = *c;
 }
 
-void test_map_should_cpy_converted_element_in_destination_for_char(int * num){
+void test_map_should_cpy_converted_element_in_destination_for_char(){
    ArrayUtil name = create(sizeof(char) ,5);
    char *n = (char *)name.base;
    n[0] = 'h',n[1] = 'e',n[2] = 'l',n[3] = 'l',n[4] = 'o';
@@ -354,14 +335,13 @@ void test_map_should_cpy_converted_element_in_destination_for_char(int * num){
    assert(d[3] == 'y');
    assert(d[4] == 'b');
    assert(strcmp(d ,"uryyb") == 0);
-   (*num)++;
 }
 
 void decrement_by(void *hint ,void *item){
    *((int *)item) = *((int *)item) - *((int *)hint);
 }
 
-void test_forEach_executes_operation_for_each_int_element(int * num){
+void test_forEach_executes_operation_for_each_int_element(){
   ArrayUtil source = create(4,5);
   int * list_array = (int *)(source.base);
   list_array[0] = 12;
@@ -378,7 +358,7 @@ void test_forEach_executes_operation_for_each_int_element(int * num){
   assert(((int *)source.base)[2] == 23);
   assert(((int *)source.base)[3] == 32);
   assert(((int *)source.base)[4] == 44);
-  (*num)++;
+
 };
 
 void to_upper(void* hint, void* item){
@@ -386,7 +366,7 @@ void to_upper(void* hint, void* item){
       *((char *)item) = *((char *)item) - 32;
 }
 
-void test_forEach_executes_operation_for_each_char_element(int *num){
+void test_forEach_executes_operation_for_each_char_element(){
   ArrayUtil source = create(1,5);
   char * list_array = (char *)(source.base);
   list_array[0] = 'a';
@@ -402,7 +382,7 @@ void test_forEach_executes_operation_for_each_char_element(int *num){
   assert(((char *)source.base)[2] == 'C');
   assert(((char *)source.base)[3] == 'D');
   assert(((char *)source.base)[4] == 'E');
-  (*num)++;
+
 };
 
 
@@ -411,52 +391,51 @@ void* add(void* hint, void* previousItem, void* item){
    return previousItem;
 }
 
-void test_reduce_reduces_the_list_and_gives_a_value(int * num){
+void test_reduce_reduces_the_list_and_gives_a_value(){
    ArrayUtil list = create(4,5);
    int * list_array = (int *)(list.base);
    list_array[0] = 1,list_array[1] = 2,list_array[2] = 3,list_array[3] = 4,list_array[4] = 5;
    int init = 10;
    int result = *(int *)reduce(list ,add ,NULL ,&init);
    assert(result == 25);
-   (*num)++;
 }
-int main() {
-   int no_of_passing_test = 0;
-
-   test_create_array_util_for_int(&no_of_passing_test);
-   test_create_array_util_for_float(&no_of_passing_test);
-   test_create_array_util_for_char(&no_of_passing_test);
-
-   test_are_equal_for_char_type_array_util(&no_of_passing_test);
-   test_are_equal_for_int_type_array_util(&no_of_passing_test);
-
-   test_resize_array_util_for_int_type(&no_of_passing_test);
-   test_resize_array_util_for_char_type(&no_of_passing_test);
-
-   test_find_index_for_int_type(&no_of_passing_test);
-   test_find_index_for_char_type(&no_of_passing_test);
-
-   test_find_first_for_int_type(&no_of_passing_test);
-   test_find_first_for_char_type(&no_of_passing_test);
-
-   test_find_last_for_int_type(&no_of_passing_test);
-   test_find_last_for_char_type(&no_of_passing_test);
-
-   test_count_for_int_type(&no_of_passing_test);
-   test_count_for_char_type(&no_of_passing_test);
-
-   test_filter_array_util_for_int_type(&no_of_passing_test);
-   test_filter_should_add_elements_into_given_array(&no_of_passing_test);
-   test_filter_should_add_the_addresses_of_elements_into_given_array(&no_of_passing_test);
-
-   test_map_should_copy_converted_element_in_destination(&no_of_passing_test);
-   test_map_should_cpy_converted_element_in_destination_for_char(&no_of_passing_test);
-
-   test_forEach_executes_operation_for_each_int_element(&no_of_passing_test);
-   test_forEach_executes_operation_for_each_char_element(&no_of_passing_test);
-
-   test_reduce_reduces_the_list_and_gives_a_value(&no_of_passing_test);
-
-   printf("no of passing tests ==>> %d\n", no_of_passing_test);
-   return 0;
-}
+// int main() {
+//    int no_of_passing_test = 0;
+//
+//    test_create_array_util_for_int(&no_of_passing_test);
+//    test_create_array_util_for_float(&no_of_passing_test);
+//    test_create_array_util_for_char(&no_of_passing_test);
+//
+//    test_are_equal_for_char_type_array_util(&no_of_passing_test);
+//    test_are_equal_for_int_type_array_util(&no_of_passing_test);
+//
+//    test_resize_array_util_for_int_type(&no_of_passing_test);
+//    test_resize_array_util_for_char_type(&no_of_passing_test);
+//
+//    test_find_index_for_int_type(&no_of_passing_test);
+//    test_find_index_for_char_type(&no_of_passing_test);
+//
+//    test_find_first_for_int_type(&no_of_passing_test);
+//    test_find_first_for_char_type(&no_of_passing_test);
+//
+//    test_find_last_for_int_type(&no_of_passing_test);
+//    test_find_last_for_char_type(&no_of_passing_test);
+//
+//    test_count_for_int_type(&no_of_passing_test);
+//    test_count_for_char_type(&no_of_passing_test);
+//
+//    test_filter_array_util_for_int_type(&no_of_passing_test);
+//    test_filter_should_add_elements_into_given_array(&no_of_passing_test);
+//    test_filter_should_add_the_addresses_of_elements_into_given_array(&no_of_passing_test);
+//
+//    test_map_should_copy_converted_element_in_destination(&no_of_passing_test);
+//    test_map_should_cpy_converted_element_in_destination_for_char(&no_of_passing_test);
+//
+//    test_forEach_executes_operation_for_each_int_element(&no_of_passing_test);
+//    test_forEach_executes_operation_for_each_char_element(&no_of_passing_test);
+//
+//    test_reduce_reduces_the_list_and_gives_a_value(&no_of_passing_test);
+//
+//    printf("no of passing tests ==>> %d\n", no_of_passing_test);
+//    return 0;
+// }
